25274fac6946713c50dde8db6e2ba25be5b82a7a
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-21 23:15:58 -0800

    fix relative path for cart-api imports

--- renderProduct.js
+++ renderProduct.js
@@ -1,6 +1,6 @@
 // import functions and grab DOM elements
 
-import { addToCart } from './cart/cart-api';
+import { addToCart } from '../cart/cart-api';
 import { violins } from './data.js';
 
 // initialize state
